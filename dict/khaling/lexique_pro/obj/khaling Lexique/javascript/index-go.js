c7de54989be58a6631396e05cdf033694c6bd3e8
Celine Buret <buret@vjf.cnrs.fr>
2013-12-30 15:30:19 +0100

    [khaling] Add Lexique Pro (generated) files

---
+++ dict/khaling/lexique_pro/obj/khaling Lexique/javascript/index-go.js
@@ -0,0 +1,7 @@
+﻿/* Go to lexical entry from Index page - and highlight it */
+
+function go(a,b) 
+{ 
+    parent.basefrm.location="../lexicon/" + a + ".htm#e" + b; 
+    parent.basefrm.fragHLload();
+}
