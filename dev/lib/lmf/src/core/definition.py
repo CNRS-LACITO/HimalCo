db5bd195d095fdf7be89c5b7d6a2f35c74de2fba
Celine Buret <buret.celine@gmail.com>
2014-10-28 17:29:37 +0100

    [lmf] Start Definition implementation

--- dev/lib/lmf/src/core/definition.py
+++ dev/lib/lmf/src/core/definition.py
@@ -3,6 +3,8 @@
 """! @package core
 """
 
+from utils.attr import check_attr_type
+
 class Definition():
     """! "Definition is a class representing a narrative description of a sense. It is provided to help human users understand the meaning of a lexical entry. A Sense instance can have zero to many definitions. Each Definition instance may be associated with zero to many Text Representation instances in order to manage the text defintion in more than one language or script. In addition, the narrative description can be expressed in a different language or script than the one in the Lexical Entry instance." (LMF)
     """
@@ -32,3 +34,62 @@ class Definition():
         for statement in self.statement:
             del statement
         del self.statement[:]
+
+    def set_language(self, language):
+        """! @brief Set language used for definition and gloss.
+        @param language Language used for definition and gloss.
+        @return Definition instance.
+        """
+        error_msg = "Language value '%s' is not allowed" % language
+        # Check value
+        check_attr_type(language, [str, unicode], error_msg)
+        self.language = language
+        return self
+
+    def get_language(self):
+        """! @brief Get language used for definition and gloss.
+        @return Definition attribute 'language'.
+        """
+        return self.language
+
+    def set_definition(self, definition, language=None):
+        """! @brief Set definition.
+        @param definition Definition.
+        @param language Language used for the definition.
+        @return Definition instance.
+        """
+        self.definition = definition
+        if language is not None:
+            self.set_language(language)
+        return self
+
+    def get_definition(self, language=None):
+        """! @brief Get definition.
+        @param language If this argument is given, get definition only if written in this language.
+        @return The filtered Definition attribute 'definition'.
+        """
+        if language is None:
+            return self.definition
+        if self.language == language:
+            return self.definition
+
+    def set_gloss(self, gloss, language=None):
+        """! @brief Set gloss.
+        @param gloss Gloss.
+        @param language Language used for the gloss.
+        @return Definition instance.
+        """
+        self.gloss = gloss
+        if language is not None:
+            self.set_language(language)
+        return self
+
+    def get_gloss(self, language=None):
+        """! @brief Get gloss.
+        @param language If this argument is given, get gloss only if written in this language.
+        @return The filtered Definition attribute 'gloss'.
+        """
+        if language is None:
+            return self.gloss
+        if self.language == language:
+            return self.gloss
--- dev/lib/lmf/utest/test_core_definition.py
+++ dev/lib/lmf/utest/test_core_definition.py
@@ -23,6 +23,68 @@ class TestDefinitionFunctions(unittest.TestCase):
         self.assertListEqual(self.definition.text_representation, [])
         self.assertListEqual(self.definition.statement, [])
 
+    def test_set_language(self):
+        lang = "Python"
+        self.assertIs(self.definition.set_language(lang), self.definition)
+        self.assertEqual(self.definition.language, lang)
+
+    def test_get_language(self):
+        # Set language
+        lang = "python"
+        self.definition.language = lang
+        # Test get language
+        self.assertEqual(self.definition.get_language(), lang)
+
+    def test_set_definition(self):
+        # Test definition only
+        definition = "blablabla"
+        self.assertIs(self.definition.set_definition(definition), self.definition)
+        self.assertEqual(self.definition.definition, definition)
+        # Test definition and language
+        definition = "This is a definition."
+        language = "eng"
+        self.assertIs(self.definition.set_definition(definition, language), self.definition)
+        self.assertEqual(self.definition.definition, definition)
+        self.assertEqual(self.definition.language, language)
+
+    def test_get_definition(self):
+        # Set definition
+        definition = "whatever"
+        self.definition.definition = definition
+        # Test get definition
+        self.assertEqual(self.definition.get_definition(), definition)
+        # Test with a language filter
+        language = "eng"
+        self.definition.language = language
+        self.assertEqual(self.definition.get_definition(), definition)
+        self.assertIsNone(self.definition.get_definition("fra"))
+        self.assertEqual(self.definition.get_definition(language), definition)
+
+    def test_set_gloss(self):
+        # Test gloss only
+        gloss = "BLA"
+        self.assertIs(self.definition.set_gloss(gloss), self.definition)
+        self.assertEqual(self.definition.gloss, gloss)
+        # Test gloss and language
+        gloss = "This is a gloss."
+        language = "eng"
+        self.assertIs(self.definition.set_gloss(gloss, language), self.definition)
+        self.assertEqual(self.definition.gloss, gloss)
+        self.assertEqual(self.definition.language, language)
+
+    def test_get_gloss(self):
+        # Set gloss
+        gloss = "WHATEVER"
+        self.definition.gloss = gloss
+        # Test get gloss
+        self.assertEqual(self.definition.get_gloss(), gloss)
+        # Test with a language filter
+        language = "eng"
+        self.definition.language = language
+        self.assertEqual(self.definition.get_gloss(), gloss)
+        self.assertIsNone(self.definition.get_gloss("fra"))
+        self.assertEqual(self.definition.get_gloss(language), gloss)
+
 suite = unittest.TestLoader().loadTestsFromTestCase(TestDefinitionFunctions)
 
 ## Run test suite
