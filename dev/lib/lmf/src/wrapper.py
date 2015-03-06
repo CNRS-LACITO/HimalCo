ec026a80c3e90d4f926018286e08c03ef48a44f7
Celine Buret <buret.celine@gmail.com>
2015-03-06 15:24:20 +0100

    [lmf] Adapt ftest

--- dev/lib/lmf/src/wrapper.py
+++ dev/lib/lmf/src/wrapper.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-"""! @package lmf.src.wrapper Contains one main function, wrapper(), which runs another function which should be a user function. If the library raises an exception, wrapper() will restore the terminal to a sane state so you can read the resulting traceback.
+"""! @package lmf.src.wrapper Contains one main function, wrapper_rw(), which runs another function which should be a user function. If the library raises an exception, wrapper_rw() will restore the terminal to a sane state so you can read the resulting traceback.
 """
 
 # Add lib/lmf/src/ folder to path
@@ -24,12 +24,13 @@ from utils.log import log
 ## Module variable
 lexical_resource = None
 
-def wrapper(func, *args, **kwds):
+def wrapper_rw(func, *args, **kwds):
     """! @brief Wrapper function that calls another function, restoring normal behavior on error.
     @param func Callable object.
     @param args Arguments passed to 'func' as its first argument.
     @param kwds Other arguments passed to 'func'.
     """
+    import wrapper
     ## As this is a user function, it is executed under 'try' statement to catch and handle exceptions
     try:
         object = func(*args, **kwds)
@@ -66,6 +67,7 @@ def wrapper(func, *args, **kwds):
         pass
 
 def read_mdf(*args, **kwds):
+    import wrapper
     # To access options
     from lmf import options
     global options
@@ -78,8 +80,8 @@ def read_mdf(*args, **kwds):
         lexicon = wrapper.lexical_resource.get_lexicon(id)
         # Add lexicon argument
         kwds.update({'lexicon': lexicon})
-    # An MDF file contains one lexicon only, but wrapper() function encapsulates it into a lexical resource
-    lexical_resource = wrapper(mdf_read, *args, **kwds)
+    # An MDF file contains one lexicon only, but wrapper_rw() function encapsulates it into a lexical resource
+    lexical_resource = wrapper_rw(mdf_read, *args, **kwds)
     for lexicon in lexical_resource.lexicon:
         if options.cross_references:
             # Verify lexicon coherence
@@ -92,7 +94,7 @@ def read_xml_lmf(*args, **kwds):
     from lmf import options
     global options
     # An XML LMF file contains one lexical resource, itself containing lexicon(s)
-    lexical_resource = wrapper(lmf_read, *args, **kwds)
+    lexical_resource = wrapper_rw(lmf_read, *args, **kwds)
     # Count total number of entries to report to user
     entries_nb = 0
     for lexicon in lexical_resource.get_lexicons():
@@ -104,24 +106,24 @@ def read_xml_lmf(*args, **kwds):
     return lexical_resource
 
 def read_sort_order(*args, **kwds):
-    sort_order = wrapper(order_read, *args, **kwds)
+    sort_order = wrapper_rw(order_read, *args, **kwds)
     log("Successfully read sort order: " + str(sort_order))
     return sort_order
 
 def read_config(*args, **kwds):
-    lexical_resource = wrapper(config_read, *args, **kwds)
+    lexical_resource = wrapper_rw(config_read, *args, **kwds)
     log("Successfully read config")
     return lexical_resource
 
 def write_mdf(*args, **kwds):
     # As an MDF file can only contain one lexicon, create as many MDF files as lexicons in the lexical resource (TODO: rename files)
     for lexicon in args[0].get_lexicons():
-        wrapper(mdf_write, lexicon, *args[1:], **kwds)
+        wrapper_rw(mdf_write, lexicon, *args[1:], **kwds)
         log("Successfully wrote %s LMF entries into MDF file '%s'." % (lexicon.count_lexical_entries(), args[1]))
 
 def write_xml_lmf(*args, **kwds):
     # An XML LMF file contains one lexical resource, itself containing lexicon(s)
-    wrapper(lmf_write, *args, **kwds)
+    wrapper_rw(lmf_write, *args, **kwds)
     # Count total number of entries to report to user
     entries_nb = 0
     for lexicon in args[0].get_lexicons():
@@ -130,7 +132,7 @@ def write_xml_lmf(*args, **kwds):
 
 def write_tex(*args, **kwds):
     # A LaTeX file contains one or several lexicons and informations about the lexical resource
-    wrapper(tex_write, *args, **kwds)
+    wrapper_rw(tex_write, *args, **kwds)
     # Count total number of entries to report to user
     entries_nb = 0
     for lexicon in args[0].get_lexicons():
@@ -141,7 +143,7 @@ def write_doc(*args, **kwds):
     # Import only when needed because it requires installation of Python package 'docx'
     from output.doc import doc_write
     # A document file contains one or several lexicons and informations about the lexical resource
-    wrapper(doc_write, *args, **kwds)
+    wrapper_rw(doc_write, *args, **kwds)
     # Count total number of entries to report to user
     entries_nb = 0
     for lexicon in args[0].get_lexicons():
