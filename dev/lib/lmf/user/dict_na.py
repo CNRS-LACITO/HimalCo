210e40e1d9e6f6925136ad9dc8d82b56e44c45bb
Celine Buret <buret.celine@gmail.com>
2015-03-10 16:00:33 +0100

    [dict] Remove end-of-lines in fields of na dictionary

--- dev/lib/lmf/user/dict_na.py
+++ dev/lib/lmf/user/dict_na.py
@@ -15,6 +15,7 @@ from setting import tex_eng, tex_fra, items
 lexical_resource = lmf.read_config(user_path + "na/config.xml")
 
 # Read MDF file and set lexicon identifier
+os.system("python " + user_path + "../src/utils/eol/eol.py -i " + user_path + "../../../../dict/na/toolbox/Dictionary.txt -o " + user_path + "na/result/dictionary-eol.txt")
 lexical_resource = lmf.read_mdf(id="na")
 
 # Display global information
