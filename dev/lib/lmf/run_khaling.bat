0592afad08f057bc53ca27cb12bd88b7ab2bb85b
Celine Buret <buret.celine@gmail.com>
2015-03-11 11:06:33 +0100

    [lmf] Clean khaling scripts for Guillaume

--- dev/lib/lmf/run_khaling.bat
+++ dev/lib/lmf/run_khaling.bat
@@ -1,22 +1,22 @@
 REM # -*- coding: utf-8 -*-
 
 REM # Go under dev/lib/lmf/ and launch this script using the following command:
-REM # run_khaling.bat
+REM # ./run_khaling.bat
 
-REM # Results are available under dev/lib/lmf/user/guillaume/ folder.
+REM # Results are available under dev/lib/lmf/user/khaling/result/ folder.
 
 REM # LaTeX file is generated with sound records and with cross references
-REM #python user/guillaume/run_khaling.py
+REM #python user/khaling/guillaume/run_khaling.py
 
 REM # LaTeX file is generated without sound records and with cross references
-REM #python user/guillaume/run_khaling.py -a
+REM #python user/khaling/guillaume/run_khaling.py -a
 
 REM # LaTeX file is generated with sound records and without cross references
-REM #python user/guillaume/run_khaling.py -c
+REM #python user/khaling/guillaume/run_khaling.py -c
 
 REM # LaTeX file is generated without sound records and without cross references
-python user/guillaume/run_khaling.py -a -c
+python user/khaling/guillaume/run_khaling.py -a -c
 
 REM # Generate PDF
-C:/texlive/2013/bin/win32/xelatex.exe -output-directory=user/guillaume/ user/guillaume/Dictionary.tex
-C:/texlive/2013/bin/win32/xelatex.exe -output-directory=user/guillaume/ user/guillaume/Dictionary.tex
+C:/texlive/2013/bin/win32/xelatex.exe -output-directory=user/khaling/result/ user/khaling/result/dictionary.tex
+C:/texlive/2013/bin/win32/xelatex.exe -output-directory=user/khaling/result/ user/khaling/result/dictionary.tex
---
+++ dev/lib/lmf/user/khaling/guillaume/run_khaling.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+
+# Go under dev/lib/lmf/ and launch this script using the following command:
+# ./user/khaling/guillaume//run_khaling.py
+
+import sys, os
+
+# Define 'user_path' as path location of lib/lmf/user/ folder
+user_path = sys.path[0] + '/../../'
+
+# Add khaling configuration folder to path
+sys.path.append(user_path + 'khaling')
+
+# Add lib/ folder to path
+sys.path.append(user_path + '../..')
+
+# Import LMF library
+import lmf
+
+# Import user customized configuration
+from setting import lmf2tex, items
+
+# Read user configuration
+lexical_resource = lmf.read_config(user_path + "khaling/guillaume/config.xml")
+
+# Run Perl script
+os.system("perl " + user_path + "../src/utils/ipa2devanagari/ipa2devanagari.pl " + user_path + "../../../../dict/khaling/toolbox/Dictionary.txt " + user_path + "khaling/result/dictionary-dev.txt")
+
+# Read MDF file and set lexicon identifier
+lexical_resource = lmf.read_mdf(id="khaling")
+
+# Classify lexicon
+sort_order = lambda character: ord(character.encode('utf-8').decode('utf-8'))
+lexical_resource.get_lexicon("khaling").sort_lexical_entries(items=items, sort_order=sort_order)
+
+# Write LaTeX file
+lmf.write_tex(lexical_resource, user_path + "khaling/result/dictionary.tex", preamble=user_path + "khaling/guillaume/khaling.tex", lmf2tex=lmf2tex, items=items, sort_order=sort_order)
+
+# Release created objects
+del lexical_resource
