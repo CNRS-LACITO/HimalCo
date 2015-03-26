CNRS-LACITO/HimalCo

commit de77e886c32af4d71ba697d4a11a3e42b30911c9

[lmf] Add links to homonyms in na PDF

dev/lib/lmf/src/output/tex.py
@@ -461,7 +461,7 @@ def format_lexical_functions(lexical_entry, font):
     # result += lexical_entry.get_lr() + " "
     return result
 
-def format_related_forms(lexical_entry, font):
+def format_related_forms(lexical_entry, font, language=None):
     """! @brief Display related forms in LaTeX format.
     @param lexical_entry The current Lexical Entry LMF instance.
     @param font A Python dictionary giving the vernacular, national, regional fonts to apply to a text in LaTeX format.
@@ -469,23 +469,35 @@ def format_related_forms(lexical_entry, font):
     """
     result = ""
     for related_form in lexical_entry.get_related_forms(mdf_semanticRelation["sy"]):
-        result += "\\textit{Syn:} "
+        if language == config.xml.French:
+            result += "\\textit{Syn~:} "
+        else:
+            result += "\\textit{Syn:} "
         if related_form.get_lexical_entry() is not None:
             result += format_link(related_form.get_lexical_entry(), font)
         else:
             result += font[VERNACULAR](related_form.get_lexeme())
         result += ". "
     for related_form in lexical_entry.get_related_forms(mdf_semanticRelation["an"]):
-        result += "\\textit{Ant:} "
+        if language == config.xml.French:
+            result += "\\textit{Ant~:} "
+        else:
+            result += "\\textit{Ant:} "
         if related_form.get_lexical_entry() is not None:
             result += format_link(related_form.get_lexical_entry(), font)
         else:
             result += font[VERNACULAR](related_form.get_lexeme())
         result += ". "
     for morphology in lexical_entry.get_morphologies():
-        result += "\\textit{Morph:} " + font[VERNACULAR](morphology) + ". "
+        if language == config.xml.French:
+            result += "\\textit{Morph~:} "
+        else:
+            result += "\\textit{Morph:} " + font[VERNACULAR](morphology) + ". "
     for related_form in lexical_entry.get_related_forms(mdf_semanticRelation["cf"]):
-        result += "\\textit{See:} "
+        if language == config.xml.French:
+            result += "\\textit{Voir~:} "
+        else:
+            result += "\\textit{See:} "
         if related_form.get_lexical_entry() is not None:
             result += format_link(related_form.get_lexical_entry(), font)
         else:

dev/lib/lmf/user/na/setting.py
@@ -220,13 +220,28 @@ def format_borrowed_word(lexical_entry, font, language):
     result = ""
     if lexical_entry.get_borrowed_word() is not None:
         if language == config.xml.French:
-            result += " \\textit{De:} "
+            result += " \\textit{De~:} "
         elif language == config.xml.English:
             result += " \\textit{From:} "
         result += lexical_entry.get_borrowed_word()
         if lexical_entry.get_written_form() is not None:
             result += " " + lexical_entry.get_written_form()
         result += ". "
+    return result
+
+def format_related_forms(lexical_entry, font, language=None):
+    import output.tex as tex
+    result = ""
+    for related_form in lexical_entry.get_related_forms(mdf_semanticRelation["hm"]):
+        if language == config.xml.French:
+            result += "\\textit{Voir~:} "
+        else:
+            result += "\\textit{See:} "
+        if related_form.get_lexical_entry() is not None:
+            result += tex.format_link(related_form.get_lexical_entry(), font)
+        else:
+            result += font[VERNACULAR](related_form.get_lexeme())
+    result += " "
     return result
 
 def tex_fra(lexical_entry, font):
@@ -253,7 +268,7 @@ def tex_fra(lexical_entry, font):
         format_borrowed_word(lexical_entry, font=config.xml.font, language=config.xml.French),\
         tex.format_examples(lexical_entry, config.xml.font, languages=[config.xml.vernacular, config.xml.French, config.xml.national]),\
         format_paradigm(lexical_entry, font=config.xml.font, language=config.xml.French),\
-        tex.format_related_forms(lexical_entry, font))
+        tex.format_related_forms(lexical_entry, font, language=config.xml.French) + format_related_forms(lexical_entry, font, language=config.xml.French))
     # Special formatting
     tex_entry = tex.handle_caps(tex_entry).replace("textsc", "mytextsc")
     # Handle reserved characters and fonts
@@ -286,7 +301,7 @@ def tex_eng(lexical_entry, font):
         format_borrowed_word(lexical_entry, font=config.xml.font, language=config.xml.English),\
         tex.format_examples(lexical_entry, config.xml.font, languages=[config.xml.vernacular, config.xml.English, config.xml.national]),\
         format_paradigm(lexical_entry, font=config.xml.font, language=config.xml.English),\
-        tex.format_related_forms(lexical_entry, font))
+        tex.format_related_forms(lexical_entry, font, language=config.xml.English) + format_related_forms(lexical_entry, font, language=config.xml.English))
     # Special formatting
     tex_entry = tex.handle_caps(tex_entry).replace("textsc", "mytextsc")
     # Handle reserved characters and fonts

dev/lib/lmf/utest/test_output_tex.py
@@ -12,11 +12,13 @@ from utils.io import EOL
 ## Test LaTeX functions
 
 ## Fonts to use in LaTeX format (output)
+FRENCH = "French"
 font = dict({
     VERNACULAR  : lambda text: "\\textbf{\ipa{" + text + "}}",
     ENGLISH     : lambda text: text,
     NATIONAL    : lambda text: "\\textit{\zh{" + text + "}}",
-    REGIONAL    : lambda text: "\ipa{" + text + "}"
+    REGIONAL    : lambda text: "\ipa{" + text + "}",
+    FRENCH      : lambda text: text
 })
 
 # To define languages and fonts
@@ -28,6 +30,7 @@ class Xml():
         self.English = "eng"
         self.national = "nat"
         self.regional = "reg"
+        self.French = "fra"
 
 class TestTexFunctions(unittest.TestCase):
 

commit 16b6daab0ca661bd62a9a748d58812a641bf7b4e

[lmf] Fix bug introduced in commit [de77e886c3]

dev/lib/lmf/src/output/tex.py
@@ -466,7 +466,7 @@ def format_lexical_functions(lexical_entry, font):
     # result += lexical_entry.get_lr() + " "
     return result
 
-def format_related_forms(lexical_entry, font, language=None):
+def format_related_forms(lexical_entry, font):
     """! @brief Display related forms in LaTeX format.
     @param lexical_entry The current Lexical Entry LMF instance.
     @param font A Python dictionary giving the vernacular, national, regional fonts to apply to a text in LaTeX format.
@@ -474,35 +474,23 @@ def format_related_forms(lexical_entry, font, language=None):
     """
     result = ""
     for related_form in lexical_entry.get_related_forms(mdf_semanticRelation["sy"]):
-        if language == config.xml.French:
-            result += "\\textit{Syn~:} "
-        else:
-            result += "\\textit{Syn:} "
+        result += "\\textit{Syn:} "
         if related_form.get_lexical_entry() is not None:
             result += format_link(related_form.get_lexical_entry(), font)
         else:
             result += font[VERNACULAR](related_form.get_lexeme())
         result += ". "
     for related_form in lexical_entry.get_related_forms(mdf_semanticRelation["an"]):
-        if language == config.xml.French:
-            result += "\\textit{Ant~:} "
-        else:
-            result += "\\textit{Ant:} "
+        result += "\\textit{Ant:} "
         if related_form.get_lexical_entry() is not None:
             result += format_link(related_form.get_lexical_entry(), font)
         else:
             result += font[VERNACULAR](related_form.get_lexeme())
         result += ". "
     for morphology in lexical_entry.get_morphologies():
-        if language == config.xml.French:
-            result += "\\textit{Morph~:} "
-        else:
-            result += "\\textit{Morph:} " + font[VERNACULAR](morphology) + ". "
+        result += "\\textit{Morph:} " + font[VERNACULAR](morphology) + ". "
     for related_form in lexical_entry.get_related_forms(mdf_semanticRelation["cf"]):
-        if language == config.xml.French:
-            result += "\\textit{Voir~:} "
-        else:
-            result += "\\textit{See:} "
+        result += "\\textit{See:} "
         if related_form.get_lexical_entry() is not None:
             result += format_link(related_form.get_lexical_entry(), font)
         else:

dev/lib/lmf/user/na/setting.py
@@ -1,9 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from config.mdf import mdf_lmf, lmf_mdf, mdf_order, ps_partOfSpeech, mdf_semanticRelation
-from config.tex import partOfSpeech_tex
-from output.tex import format_definitions
+from config.mdf import mdf_lmf, lmf_mdf, mdf_semanticRelation
 from utils.io import EOL
 from common.defs import VERNACULAR, ENGLISH, NATIONAL, REGIONAL
 
@@ -256,6 +254,41 @@ def format_paradigm(lexical_entry, font, language):
 def format_related_forms(lexical_entry, font, language=None):
     import output.tex as tex
     result = ""
+    for related_form in lexical_entry.get_related_forms(mdf_semanticRelation["sy"]):
+        if language == config.xml.French:
+            result += "\\textit{Syn~:} "
+        else:
+            result += "\\textit{Syn:} "
+        if related_form.get_lexical_entry() is not None:
+            result += tex.format_link(related_form.get_lexical_entry(), font)
+        else:
+            result += font[VERNACULAR](related_form.get_lexeme())
+        result += ". "
+    for related_form in lexical_entry.get_related_forms(mdf_semanticRelation["an"]):
+        if language == config.xml.French:
+            result += "\\textit{Ant~:} "
+        else:
+            result += "\\textit{Ant:} "
+        if related_form.get_lexical_entry() is not None:
+            result += tex.format_link(related_form.get_lexical_entry(), font)
+        else:
+            result += font[VERNACULAR](related_form.get_lexeme())
+        result += ". "
+    for morphology in lexical_entry.get_morphologies():
+        if language == config.xml.French:
+            result += "\\textit{Morph~:} "
+        else:
+            result += "\\textit{Morph:} " + font[VERNACULAR](morphology) + ". "
+    for related_form in lexical_entry.get_related_forms(mdf_semanticRelation["cf"]):
+        if language == config.xml.French:
+            result += "\\textit{Voir~:} "
+        else:
+            result += "\\textit{See:} "
+        if related_form.get_lexical_entry() is not None:
+            result += tex.format_link(related_form.get_lexical_entry(), font)
+        else:
+            result += font[VERNACULAR](related_form.get_lexeme())
+        result += " "
     for related_form in lexical_entry.get_related_forms(mdf_semanticRelation["hm"]):
         if language == config.xml.French:
             result += "\\textit{Voir~:} "
@@ -265,7 +298,11 @@ def format_related_forms(lexical_entry, font, language=None):
             result += tex.format_link(related_form.get_lexical_entry(), font)
         else:
             result += font[VERNACULAR](related_form.get_lexeme())
-    result += " "
+        result += " "
+    # ce
+    # cn
+    # cr
+    # result += "\\textit{See main entry:} " + font[VERNACULAR](lexical_entry.get_mn()) + ". "
     return result
 
 def tex_fra(lexical_entry, font):
@@ -293,7 +330,7 @@ def tex_fra(lexical_entry, font):
         format_borrowed_word(lexical_entry, font=config.xml.font, language=config.xml.French),\
         tex.format_examples(lexical_entry, config.xml.font, languages=[config.xml.vernacular, config.xml.French, config.xml.national]),\
         format_paradigm(lexical_entry, font=config.xml.font, language=config.xml.French),\
-        tex.format_related_forms(lexical_entry, font, language=config.xml.French) + format_related_forms(lexical_entry, font, language=config.xml.French))
+        format_related_forms(lexical_entry, font, language=config.xml.French))
     # Special formatting
     tex_entry = tex.handle_caps(tex_entry).replace("textsc", "mytextsc")
     # Handle reserved characters and fonts
@@ -327,7 +364,7 @@ def tex_eng(lexical_entry, font):
         format_borrowed_word(lexical_entry, font=config.xml.font, language=config.xml.English),\
         tex.format_examples(lexical_entry, config.xml.font, languages=[config.xml.vernacular, config.xml.English, config.xml.national]),\
         format_paradigm(lexical_entry, font=config.xml.font, language=config.xml.English),\
-        tex.format_related_forms(lexical_entry, font, language=config.xml.English) + format_related_forms(lexical_entry, font, language=config.xml.English))
+        format_related_forms(lexical_entry, font, language=config.xml.English))
     # Special formatting
     tex_entry = tex.handle_caps(tex_entry).replace("textsc", "mytextsc")
     # Handle reserved characters and fonts

dev/lib/lmf/utest/test_output_tex.py
@@ -12,13 +12,11 @@ from utils.io import EOL
 ## Test LaTeX functions
 
 ## Fonts to use in LaTeX format (output)
-FRENCH = "French"
 font = dict({
     VERNACULAR  : lambda text: "\\textbf{\ipa{" + text + "}}",
     ENGLISH     : lambda text: text,
     NATIONAL    : lambda text: "\\textit{\zh{" + text + "}}",
-    REGIONAL    : lambda text: "\ipa{" + text + "}",
-    FRENCH      : lambda text: text
+    REGIONAL    : lambda text: "\ipa{" + text + "}"
 })
 
 # To define languages and fonts
@@ -30,7 +28,6 @@ class Xml():
         self.English = "eng"
         self.national = "nat"
         self.regional = "reg"
-        self.French = "fra"
 
 class TestTexFunctions(unittest.TestCase):
 