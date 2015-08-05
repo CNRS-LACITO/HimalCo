fa4aec5636db1862c4aaa5130279221e07fe910e
Guillaume Jacques <rgyalrongskad@gmail.com>
2015-08-05 15:04:02 +0200

    [doc] evd

--- dev/lib/lmf/run_khaling.bat
+++ dev/lib/lmf/run_khaling.bat
@@ -6,7 +6,7 @@
 :: Results are available under dev/lib/lmf/user/khaling/result/ folder.
 
 :: LaTeX file is generated with sound records and with cross references
-python user/khaling/guillaume/run_khaling.py
+:: python user/khaling/guillaume/run_khaling.py
 
 :: LaTeX file is generated without sound records and with cross references
 ::python user/khaling/guillaume/run_khaling.py -a
@@ -15,7 +15,7 @@ python user/khaling/guillaume/run_khaling.py
 ::python user/khaling/guillaume/run_khaling.py -c
 
 :: LaTeX file is generated without sound records and without cross references
-::python user/khaling/guillaume/run_khaling.py -a -c
+python user/khaling/guillaume/run_khaling.py -a -c
 
 :: Generate PDF: add xelatex binary location to your PATH environment variable
 xelatex.exe -output-directory=user/khaling/result/ user/khaling/result/dictionary.tex --halt-on-error=N
