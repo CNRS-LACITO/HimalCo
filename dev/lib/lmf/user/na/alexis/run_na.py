e6a4e7d146a14cf4674c75c39d4c1797dd4249e8
alexis-michaud <michaud.cnrs@gmail.com>
2015-10-09 17:39:00 +0700

    [Na] màj. Version avec proposition d'orthographe
    Version avec proposition d'orthographe pour toutes les entrées

--- dev/lib/lmf/user/na/alexis/run_na.py
+++ dev/lib/lmf/user/na/alexis/run_na.py
@@ -41,7 +41,7 @@ classify_lexicon(lexical_resource.get_lexicon("na"), xml_order, xml_type)
 os.system("python " + user_path + "../src/utils/tables/tables.py -i " + user_path + "../../../../dict/na/toolbox/Dictionary.txt -e " + user_path + "na/result/table_eng.tex -f " + user_path + "na/result/table_fra.tex")
 
 # Write LaTeX files
-lmf.write_tex(lexical_resource, user_path + "na/result/dictionary_eng.tex", preamble=user_path + "na/preamble.tex", introduction=user_path + "na/introduction_eng.tex", lmf2tex=tex_eng, items=items, sort_order=xml_order, tables=[user_path + "na/result/table_eng.tex"], title="Online Na-English-Chinese-French Dictionary (version 1.0)")
+lmf.write_tex(lexical_resource, user_path + "na/result/dictionary_eng.tex", preamble=user_path + "na/preamble.tex", introduction=user_path + "na/introduction_eng.tex", lmf2tex=tex_eng, items=items, sort_order=xml_order, tables=[user_path + "na/result/table_eng.tex"], title="Online Na-English-Chinese Dictionary (version 1.0)")
 lmf.write_tex(lexical_resource, user_path + "na/result/dictionary_fra.tex", preamble=user_path + "na/preamble.tex", introduction=user_path + "na/introduction_fra.tex", lmf2tex=tex_fra, items=items, sort_order=xml_order, tables=[user_path + "na/result/table_fra.tex"], title=u"Dictionnaire na-chinois-français en ligne (version 1.0)", tex_language="french")
 
 # Release created objects
