7453f41a01e9d865bad7c8c0b541fb3122ee6f69
Guillaume Jacques <rgyalrongskad@gmail.com>
2015-12-08 15:04:37 +0100

    [gitignore] dictionary.txt

--- dev/scripts/tools/degrader_wav_to__mp3.bat
+++ dev/scripts/tools/degrader_wav_to__mp3.bat
@@ -1,1 +1,1 @@
-for %%a in ("*.wav") do ffmpeg -i "%%a" -ab 128k -ar 44100 "%%~na.mp3"
+for %%a in ("C:\Users\crlao\Documents\GitHub\HimalCo\dict\japhug\data\audio\new\*.wav") do ffmpeg -i "%%a" -ab 128k -ar 22005 "C:\Users\crlao\Desktop\journee\%%~na.mp3"
