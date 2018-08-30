56d43e5ee41fcf0bd3649b282177db188086aa8e
Guillaume Jacques <rgyalrongskad@gmail.com>
2018-08-30 13:18:37 +0200

    suppression des fichiers de Guillaume Jacques

--- dict/japhug/lexique_pro/obj/japhug Lexique/javascript/index-go.js
+++
@@ -1,7 +0,0 @@
-﻿/* Go to lexical entry from Index page - and highlight it */
-
-function go(a,b) 
-{ 
-    parent.basefrm.location="../lexicon/" + a + ".htm#e" + b; 
-    parent.basefrm.fragHLload();
-}
--- dict/japhug/toolbox/Settings/ReadmeAfter.txt
+++
@@ -1,3 +0,0 @@
-A shortcut to the new Toolbox project has been placed on your desktop.
-Its icon looks like an open toolbox. Its name is "Toolbox Project".
-
